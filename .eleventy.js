dc3294079b11b80454e5a8be59f507f4af3d33fd
Sandeep Singh <serperior2@protonmail.com>
2023-11-21 19:21:10 -0500

    add exp section, style tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/img");
     eleventyConfig.addPassthroughCopy("src/scripts");
+    eleventyConfig.addPassthroughCopy("src/files");
 
     // Input Directory : src, Output directory: _site
     return {

d9448a2adc86d923eb794240e91d6b7867d70500
Sandeep Singh <serperior2@protonmail.com>
2023-12-19 23:53:48 -0500

    remove merged 11ty data

--- src/_data/project.js
+++
@@ -1,8 +0,0 @@
-// set project url
-let projectUrl = 'https://sandeepdev.com';
-if (process.env.ELEVENTY_ENV === 'dev') projectUrl = 'http://localhost:8080';
-
-module.exports = {
-    env: process.env.ELEVENTY_ENV,
-    url: projectUrl
-};

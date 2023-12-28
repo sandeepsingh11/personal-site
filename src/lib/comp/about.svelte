e719cda0645f6732cb7994e29438af71fe60f242
Sandeep Singh <serperior2@protonmail.com>
2023-12-27 22:11:54 -0500

    minify portrait image

--- src/lib/comp/about.svelte
+++ src/lib/comp/about.svelte
@@ -6,15 +6,12 @@
 
     <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-4 w-4/5 mx-auto mb-4">
         <div class="w-[333px] max-w-[227px] md:max-w-[100%] mx-auto mb-8">
-            <img src="/sandeep_portrait.jpg" alt="portrait of Sandeep" class="rounded-md">
+            <img src="/sandeep_portrait_min.jpg" alt="portrait of Sandeep" class="rounded-md">
         </div>
         <div>
             <p class="leading-loose mb-4">Need a website but don't know who to go to? Have an existing site but want an overhaul? Message me to get started!<br><br>I believe the simple things in life are the most valuable. The same goes for websites. Simple technology and a clean design will have the strongest impression towards your visitors. I take this approach as it saves development time, reduces hosting costs, outputs a lighter website, and dramatically speeds up page load time. Work with me to make the internet great again!</p>
 
-            <a href="#contact" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-transparent hover:bg-accent-500 text-accent-500 hover:text-black rounded border-2 border-accent-500 focus:ring-2 focus:outline-none focus:ring-accent-300">
-                Get started!
-                <i data-feather="arrow-right" stroke="currentColor" stroke-width="2" class="mx-auto"></i>
-            </a>
+            <a href="#contact" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-transparent hover:bg-accent-500 text-accent-500 hover:text-black rounded border-2 border-accent-500 focus:ring-2 focus:outline-none focus:ring-accent-300">Get started!</a>
         </div>
     </div>
 </div>

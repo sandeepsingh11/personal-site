0947183e9cc0abad70ec57fe8958fe3c5608eaf7
Sandeep Singh <serperior2@protonmail.com>
2023-12-27 21:40:35 -0500

    restyle Footer

--- src/lib/comp/footer.svelte
+++ src/lib/comp/footer.svelte
@@ -1,22 +1,29 @@
 <script>
-    import { onMount } from 'svelte';
+    import { Mail, Github, Linkedin } from "lucide-svelte";
 
+    /** @type {import('$lib/server/socials.json')} */
     export let socials;
 
-    onMount(() => {
-        // copyright year
-        document.getElementById('year').innerText = String(new Date().getFullYear());
-    })
+    export const year = new Date().getFullYear();
 </script>
 
 <footer class="bg-black text-white flex justify-between px-4 md:px-8 py-2">
     <div>
-        <p>&copy <span id="year"></span> Coded by <a href="https://github.com/sandeepsingh11/" target="_blank" rel="noopener noreferrer">Sandeep</a></p>
+        <p>&copy { (year) ? year : '2024' } Coded by <a href="https://github.com/sandeepsingh11/" target="_blank" rel="noopener noreferrer">Sandeep</a></p>
     </div>
 
     <div class="grid grid-cols-3 gap-4">
         {#each socials as social}
-            <a href="{ social.url }" target="_blank" rel="noopener noreferrer"><img src="/icons/{social.icon}.svg" alt="{social.icon}"></a>
+            <a href="{ social.url }" target="_blank" rel="noopener noreferrer">
+                <!-- <img src="/icons/{social.icon}.svg" alt="{social.icon}"> -->
+                {#if social.icon === 'mail'}
+                    <Mail />
+                {:else if social.icon === 'github'}
+                    <Github />
+                {:else if social.icon === 'linkedin'}
+                    <Linkedin />
+                {/if}
+            </a>
         {/each}
     </div>
 </footer>

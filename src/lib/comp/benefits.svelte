b9fafe48262d87300b8976cea749e32167cd469b
Sandeep Singh <serperior2@protonmail.com>
2023-12-27 21:33:28 -0500

    add Lucide icons, restyle Benefits section

--- src/lib/comp/benefits.svelte
+++ src/lib/comp/benefits.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { Feather, Smartphone, Zap, Pen, Code, Wrench } from "lucide-svelte";
+
+    /** @type {import('$lib/server/benefits.json')} */
     export let benefits;
 </script>
 
@@ -10,8 +13,20 @@
 
     <ul class="grid grid-cols-2 md:grid-cols-3 justify-items-center leading-relaxed">
         {#each benefits as benefit}
-            <li class="text-center mb-12 p-2 w-4/5">
-                <img src="/icons/{benefit.icon}.svg" alt="{benefit.icon}" class="w-[100px] h-[100px] mx-auto mb-1">
+            <li class="bg-primary-600 w-4/5 mb-12 py-4 px-2 border-2 border-black rounded text-center text-white">
+                {#if benefit.icon === 'feather'}
+                    <Feather size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {:else if benefit.icon === 'smartphone'}
+                    <Smartphone size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {:else if benefit.icon === 'zap'}
+                    <Zap size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {:else if benefit.icon === 'pen'}
+                    <Pen size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {:else if benefit.icon === 'code'}
+                    <Code size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {:else if benefit.icon === 'wrench'}
+                    <Wrench size=100 strokeWidth=1 class="mx-auto mb-1" />
+                {/if}
                 <h4 class="mb-3 text-xl md:text-2xl font-medium">{ benefit.title }</h4>
                 <p>{ benefit.desc }</p>
             </li>
